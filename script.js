0756880260ee60064815a5ff1b4f1656d5c30625
Ricalb <48953707+Ricalb@users.noreply.github.com>
2019-03-26 09:33:46 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+
+$("button").click(function() {
+  $("#chest").show();
+});
+
+$(".button").click(function() {
+  $("man").hide();
+});
+$("button").dblclick(function() {
+  $("#man").show();
+});
+$("#hide").click(function(){
+  $("h1").hide();
+});
+$("button").click(function() {
+  $("#treasure").show();
+});
+
+
+
+
+
+
+
+
+
+
+
+
